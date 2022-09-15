5a93bc25c44a659a85a0a420e21ca094a29d3b2a
Hiago Murilo Lima Indalecio <hiagoindalecio@outlook.com>
2022-09-15 19:07:28 -0300

    Creating the partners controller

---
+++ Controllers/Partner/PartnersController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Samauma.Domain.Errors;
+
+namespace Samauma.Controllers.Partner
+{
+    [ApiController]
+    [Route("Partners")]
+    [Authorize]
+    public class PartnersController : ControllerBase
+    {
+        private readonly ILogger<PartnersController> _logger;
+
+        public PartnersController(
+            ILogger<PartnersController> logger
+            )
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public async Task<ObjectResult> CreatePartner(/*[FromBody] CreatePartnerInput Input*/)
+        {
+            //_logger.LogInformation("Create partner Called => {Name}", Input.Name);
+
+            try
+            {
+                return new ObjectResult(/*Data*/ new { });
+            }
+            catch (BaseException e)
+            {
+                return new BadRequestObjectResult(e.Data);
+            }
+            catch (Exception e)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, e.Message);
+            }
+        }
+    }
+}
