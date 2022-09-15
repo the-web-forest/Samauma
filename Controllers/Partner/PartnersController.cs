e445a6349dfcf65b0c66fa9ca838efe2becc9347
Hiago Murilo Lima Indalecio <hiagoindalecio@outlook.com>
2022-09-15 19:54:31 -0300

    Creating the partners controller structure

--- Controllers/Partner/PartnersController.cs
+++ Controllers/Partner/PartnersController.cs
@@ -13,19 +13,24 @@ namespace Samauma.Controllers.Partner
 
         public PartnersController(
             ILogger<PartnersController> logger
-            )
+        )
         {
             _logger = logger;
         }
 
-        [HttpPost]
-        public async Task<ObjectResult> CreatePartner(/*[FromBody] CreatePartnerInput Input*/)
+        /*[HttpPost]
+        public async Task<ObjectResult> CreatePartner([FromBody] CreatePartnerInput Input/)
         {
-            //_logger.LogInformation("Create partner Called => {Name}", Input.Name);
+            _logger.LogInformation("Create partner has called => {Name}", Input.Name);
 
             try
             {
-                return new ObjectResult(/*Data*/ new { });
+                var Data = await _createPartnerUseCase.Run(new CreatePartnerUseCaseInput
+                {
+                    
+                });
+
+                return new ObjectResult( new { });
             }
             catch (BaseException e)
             {
@@ -35,6 +40,101 @@ namespace Samauma.Controllers.Partner
             {
                 return StatusCode(StatusCodes.Status500InternalServerError, e.Message);
             }
-        }
+        }*/
+
+        /*[HttpPut]
+        public async Task<ObjectResult> UpdatePartner([FromBody] UpdatePartnerInput Input)
+        {
+            _logger.LogInformation("Update partner has called => {Id}", Input.Id);
+            
+            try
+            {
+                var Data = await _updatePartnerUseCase.Run(new UpdatePartnerUseCaseInput
+                {
+                    
+                });
+
+                return new ObjectResult(Data);
+            }
+            catch (BaseException e)
+            {
+                return new BadRequestObjectResult(e.Data);
+            }
+            catch (Exception e)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, e.Message);
+            }
+        }*/
+
+        /*[HttpGet("List")]
+        public async Task<ObjectResult> GetTree([FromQuery] int Page)
+        {
+            _logger.LogInformation("Get partner list has called, Page => {Page}", Page);
+
+            try
+            {
+                var Data = await _listPartnersUseCase.Run(new ListPartnersUseCaseInput
+                {
+                    
+                });
+
+                return new ObjectResult(Data);
+            }
+            catch (BaseException e)
+            {
+                return new BadRequestObjectResult(e.Data);
+            }
+            catch (Exception e)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, e.Message);
+            }
+        }*/
+
+        /*[HttpGet("{Id}")]
+        public async Task<ObjectResult> GetPartnerById(string Id)
+        {
+            _logger.LogInformation("Get partner by identificator has called => {Id}", Id);
+            try
+            {
+                var Data = await _getPartnerByIdUseCase.Run(new GetPartnerByIdUseCaseInput
+                {
+                    
+                });
+
+                return new ObjectResult(Data);
+            }
+            catch (BaseException e)
+            {
+                return new BadRequestObjectResult(e.Data);
+            }
+            catch (Exception e)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, e.Message);
+            }
+        }*/
+
+        /*[HttpDelete("{Id}")]
+        public async Task<ObjectResult> DeletePartner(string Id)
+        {
+            _logger.LogInformation("Delete partner has called => {Id}", Id);
+
+            try
+            {
+                var Data = await _deletePartnerUseCase.Run(new DeletePartnerUseCaseInput
+                {
+                    Id = Id
+                });
+
+                return new ObjectResult(Data);
+            }
+            catch (BaseException e)
+            {
+                return new BadRequestObjectResult(e.Data);
+            }
+            catch (Exception e)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, e.Message);
+            }
+        }*/
     }
 }
