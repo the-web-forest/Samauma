d10bf0a59dfe0723ed3be60885e0492052209d78
Hiago Murilo Lima Indalecio <hiagoindalecio@outlook.com>
2022-09-27 20:36:09 -0300

    Solving code smell

--- Domain/Exceptions/PartnerExceptions/EmailAlreadyRegistered.cs
+++ Domain/Exceptions/PartnerExceptions/EmailAlreadyRegistered.cs
@@ -1,10 +1,12 @@
-﻿namespace Samauma.Domain.Errors
+﻿using System.Runtime.Serialization;
+
+namespace Samauma.Domain.Errors
 {
     [Serializable]
     public class EmailAlreadyRegisteredException : BaseException
     {
         public EmailAlreadyRegisteredException() : base("012", "Partner Email Already Registered") { }
 
-        protected EmailAlreadyRegisteredException(string Code, string Message) : base(Code, Message) { }
+        protected EmailAlreadyRegisteredException(SerializationInfo info, StreamingContext context) : base("012", "Partner Email Already Registered") { }
     }
 }
